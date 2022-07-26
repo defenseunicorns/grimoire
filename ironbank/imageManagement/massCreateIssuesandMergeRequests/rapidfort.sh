92b69ff601d71d026865a7d2ae3002ccc25f2f67
Lucas Rodriguez <lucas@Lucass-MacBook-Pro.local>
2022-07-25 22:45:50 -0500

    Update repo1 token variable to accept a command line argument as input

--- ironbank/imageManagement/massCreateIssuesandMergeRequests/rapidfort.sh
+++ ironbank/imageManagement/massCreateIssuesandMergeRequests/rapidfort.sh
@@ -5,19 +5,18 @@ set -ex
 GITLAB_PROJECTS_API_ENDPOINT="https://repo1.dso.mil/api/v4/projects"
 PROJECT_IDS=( "7434" "7435" "7431" "7432" "7430" "7429" "7428" "7433" )
 IMAGE_NAMES=( "aggregator-exe" "backend-exe" "frontrow" "iso-master-exe" "rfapi-exe" "rf-scan-exe" "rfpubsub-exe" "runner" )
-REPO1_TOKEN="enter_your_token_here" # Add your Repo1 token. Reminder to NOT commit/push credentials.
+REPO1_TOKEN="$1"
 
 function get_image_digests() {
     IMAGE_REGISTRY="public.ecr.aws"
     IMAGE_REPO="rapidfort"
-    IMAGE_TAG="1.1.11-rfhardened" # Update as needed.
+    IMAGE_TAG="1.1.16-rfhardened" # Update as needed.
     
     echo "Collecting image digests for Rapidfort images..."
 
     for image in "${IMAGE_NAMES[@]}"
     do
         IMAGE_DIGEST=$(skopeo inspect docker://${IMAGE_REGISTRY}/${IMAGE_REPO}/${image}:${IMAGE_TAG} --no-tags | jq -r '.Digest')
-        IMAGE_DIGESTS+=( ${IMAGE_DIGEST} )
         echo "Update ${image} to ${IMAGE_DIGEST}" >> rapidfort-image-digests.txt
     done
 
